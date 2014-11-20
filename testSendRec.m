030ba4334386cf23c2f67b7df0f20d0dc7c6c963
Potnuss <Potnuss@gmail.com>
2014-11-20 09:20:40 +0100

    Removed and simplified things in testSendRec

--- testSendRec.m
+++ testSendRec.m
@@ -10,21 +10,14 @@ for h = 1:N
     % Svector(h) = b(bitSeq(h),bitSeq(h+1));
     Svector(h) = b(bitSeq(2*h-1),bitSeq(2*h));
 end
-% Generate z(n)
-z = zeros(1,N);
-for n = 0:N-1
-    for k = 0:N-1
-        z(n+1) = z(n+1) + (1/N)*Svector(k+1)*exp(1i*2*pi*k*n/N);
-    end
-end
-Fs = ifft(Svector);
-% cyclic_prefix 60
+% OFDM - Generate z(n)
+z = ifft(Svector);
 
+% add cyclic_prefix to signal
 cyclic_prefix = z(end-cycP+1:end);
 zz = [cyclic_prefix z].';
 
-% Discrete Time impulse response of H1
-
+% Channel select
 h1 = zeros(1,60);
 for n = 0:59
     h1(n+1) = 0.8^(n);
@@ -50,7 +43,7 @@ w = 1/sqrt(2)*sigma*(randn(y_len,1) + 1i*randn(y_len,1));
 % Filter through channel
 y = conv(h, zz) + w;
 
-% OFDM
+% OFDM^1
 % FFT the last 128 of the signal
 
 r = fft(y((cycP+1):128+cycP));
